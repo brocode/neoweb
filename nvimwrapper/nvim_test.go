c9872ed37ce1063a4805df862f7dcec7104ef769
Patrick Haun <patrick@bomgar.email>
2024-10-11 13:51:51 +0200

    test: render hl runes

--- nvimwrapper/nvim_test.go
+++ nvimwrapper/nvim_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/brocode/neoweb/key"
+	"github.com/brocode/neoweb/nvimwrapper/raster"
 	"github.com/stretchr/testify/require"
 )
 
@@ -48,4 +49,24 @@ func TestTranslateModifiers(t *testing.T) {
 			CtrlKey:  true,
 			ShiftKey: true,
 		}))
+}
+
+func TestRenderHlRunes(t *testing.T) {
+	r := raster.New[hlRune]()
+	r.Resize(4, 1)
+	r.Put(0, 0, []hlRune{
+		{rune: 'f', hlId: 1},
+		{rune: 'k', hlId: 1},
+		{rune: 'b', hlId: 1},
+		{rune: 'r', hlId: 2},
+	})
+	lines := renderHlRunes(r)
+	require.Equal(t, 1, len(lines), "Only one line")
+
+	line := lines[0]
+
+	require.Equal(t, 2, len(line.Spans), "2 hl ids")
+	require.Equal(t, "fkb", line.Spans[0].Text)
+	require.Equal(t, 1, line.Spans[0].HlId)
+	require.Equal(t, 2, line.Spans[1].HlId)
 }
